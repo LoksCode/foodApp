1d6d2d3256ac73fad77db0f55c2427fb2460d26e
Dawid Lokś <dawid.loks@icloud.com>
2024-04-12 16:53:48 +0200

    added custom error component and http hook to handle request waiting

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Meals from './Components/Meals.jsx';
 import { CartContextProvider } from './store/CartContext.jsx';
 import { UserProgressContextProvider } from './store/UserProgressContext.jsx';
 import Cart from './Components/Cart.jsx';
+import Checkout from './Components/Checkout.jsx';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Meals />
         <Cart />
+        <Checkout />
       </CartContextProvider>
     </UserProgressContextProvider>
   );
